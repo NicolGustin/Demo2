cc89b7976641adae27ed145db4d937031509e7e4
Nicol Gustin <nicol930519@hotmail.com>
2024-04-14 16:53:03 -0500

    MyConsole

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using ConsoleApp;
+using MyLibrary;
+
+Employee Nicol = new Secretary();
+Nicol.Name = "Nicol";
+Employee Juan = new Manager();
+Juan.Name = "Juan";
+Developer Vanessa = new Developer();
+Console.WriteLine($"Dias: {Vanessa.GetPaymentDays()}");
+
+WriteEmployeeInfo(Nicol);
+WriteEmployeeInfo(Juan);
+WriteEmployeeInfo(Vanessa);
+
+void WriteEmployeeInfo(Employee employee)
+{
+    Console.WriteLine($"{employee.Name} : {employee.CalculateMonthlpayment()}");
+    Console.WriteLine($" Days: {employee.GetPaymentDays()}");
+}
