46e84c548bf7201513f824fc5474931de7232080
Nicol Gustin <nicol930519@hotmail.com>
2024-04-14 17:04:35 -0500

    Employee

---
+++ MyLibrary/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int GetInt()
+        {
+            return 5;
+        }
+
+        public virtual decimal CalculateMonthlpayment()
+        {
+            return 12000;
+        }
+
+        public int GetPaymentDays()
+        {
+            return 30;
+        }
+    }
+
+    public class Secretary : Employee
+    {
+        public override decimal CalculateMonthlpayment() => 15000;
+    }
+    public class Manager : Employee
+    {
+        public override decimal CalculateMonthlpayment()
+        {
+            Return 30000;
+        }
+    }
+
+    public class Developer : Employee
+    {
+        public new int GetPaymentDays()
+        {
+            return 20;
+        }
+    }
+}
